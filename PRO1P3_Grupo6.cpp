ce653066e7d71987bcb415c76ee8e77cc87644f8
Tatiana-Garcia <tzgarcia_16@unitec.edu>
2023-06-19 13:05:29 -0600

    Ni modo, voy a tuto

--- PRO1P3_Grupo6.cpp
+++ PRO1P3_Grupo6.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <stdlib.h>
-//#include "Funciones.h"
+#include<string>
+#include "Funciones.h"
+using namespace std;
 
 int main(int argc, char* argv[]){
-    /*string figura = atoi(argv[1]);
-    Funciones f;*/
-    /*f.lectura(figura, argv[]);*/
+    /*string figura = atoi(argv[1]); 
+    Funciones f; 
+    f.lectura(figura, argv[]);*/ 
 }
