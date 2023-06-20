08cdec3ba300bb676d944f7ea99633249d4e085f
Andrea Ortez <andreaortez11@unitec.edu>
2023-06-19 22:32:42 -0600

    rombo

--- Rombo.cpp
+++ Rombo.cpp
@@ -1,5 +1,25 @@
 #include "Rombo.h"
 
-void Rombo::leerRombo(int a, int d, int D)
-{
+void Rombo::leerRombo(int a, int d, int D){
+    ifstream rombo;
+    string texto;
+
+    rombo.open("Rombo.txt", ios::in);
+    if (rombo.fail()) {
+        cout << "No se pudo abrir el archivo";
+        exit(1);
+    }
+
+    while (!rombo.eof()) {
+        getline(rombo, texto);
+        for (int i = 0; i < texto.size(); i++){
+            if (texto[i]==123 && texto[i+1]==97)
+                texto[i] = a;
+            if (texto[i] == 123 && texto[i + 1] == 68)
+                texto[i] = D;
+            if (texto[i] == 123 && texto[i + 1] == 100)
+                texto[i] = d;
+        }
+        cout << texto << endl;
+    }
 }
--- Rombo.h
+++ Rombo.h
@@ -1,6 +1,10 @@
 #pragma once
-class Rombo
-{
+#include <fstream>
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Rombo{
 public: 
 	static void leerRombo(int a, int d, int D);
 	void Dibujar();
