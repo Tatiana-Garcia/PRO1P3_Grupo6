ae43fa11fef66ecae2e17aba28924612f737d926
Tatiana-Garcia <tzgarcia_16@unitec.edu>
2023-06-19 19:15:06 -0600

    Main corregido?

--- PRO1P3_Grupo6.cpp
+++ PRO1P3_Grupo6.cpp
@@ -1,11 +1,8 @@
 #include <iostream>
 #include <stdlib.h>
-#include<string>
 #include "Funciones.h"
-using namespace std;
 
 int main(int argc, char* argv[]){
-    /*string figura = atoi(argv[1]); 
-    Funciones f; 
-    f.lectura(figura, argv[]);*/ 
+    string figura = argv[1];   
+    lectura(figura, argv);  
 }
