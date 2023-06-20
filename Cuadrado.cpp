cbe4b2c11de44e4704b7569a053c3e4a33e1d5f5
Tatiana-Garcia <tzgarcia_16@unitec.edu>
2023-06-20 11:14:18 -0600

    Cuadrado se pudo, solo tendria que hacer validaciones

--- Cuadrado.cpp
+++ Cuadrado.cpp
@@ -2,9 +2,14 @@
 
 void Cuadrado::leerCuadrado(int a)
 {
-    string str = R"(\[a = \{a\}\])", str2 = R"([\[\]])", str3 = R"( a )", str4 = R"([\{\}])";
-    regex patron(str), patron2(str2), patron3(str3), patron4(str4);  
-    string linea, print =" a = "+ to_string(a)+" ", print2 = " ";
+    int area = a*a, perimetro = 4*a; 
+    string str5 = R"(\|       4\*a       \|      a\*a      \|)";
+    string str = R"(\[a = \{a\}\])", 
+        str2 = R"(\|    4\*\(\[\{a\}\]\)    \| \[\{a\}\] \^ \[\{2\}\] \|)", 
+        str3 = R"(\|     \[\{4\*a\}\]     \|    \[\{a\*a\}\]    \|)", 
+        str4 = R"([\{\}])";
+    regex patron(str), patron2(str2), patron3(str3), patron4(str4), patron5(str5); 
+    string linea, print =" a = "+ to_string(a)+" ";
     ifstream Entrada("Cuadrado.txt", ios::in);
     if (!Entrada) {
         cerr << "No se pudo abrir el archivo" << endl;
@@ -21,12 +26,18 @@ void Cuadrado::leerCuadrado(int a)
                 cout << resultado << endl; 
             }
             else if (regex_search(linea, matches, patron2)) { //linea para modificar desarrollo de area y perimetro con llaves de cuadrado 
-                string llaves = regex_replace(linea, patron2, print2); 
-                string llaves2 = regex_replace(llaves, patron4, print2);
-                string cadena = R"(\{a\})";
-                regex letras(cadena); 
-                string resultado = regex_replace(llaves2, letras, to_string(a)); 
-                cout << resultado << endl;
+                
+                string print2 = "|    4*( "+to_string(a) + " )    |    "+to_string(a) + " ^ 2    |";
+                string llaves = regex_replace(linea, patron2, print2);
+                cout << llaves << endl; 
+
+            }
+            else if (regex_search(linea, matches, patron3)) { //linea para modificar desarrollo de area y perimetro con llaves de cuadrado 
+
+                string print3 = "|       "+to_string(perimetro) + "      |     "+to_string(area) + "    |";
+                string llaves = regex_replace(linea, patron3, print3);
+                cout << llaves << endl;
+
             }//faltara un if para las multiplicaciones
             else {
                 cout << linea << endl;
