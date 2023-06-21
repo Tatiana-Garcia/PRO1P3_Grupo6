9fb3c8402eb1bd23962ff57261412a0535a4b26d
Tatiana-Garcia <tzgarcia_16@unitec.edu>
2023-06-20 18:19:15 -0600

    Parte cometa

--- Cometa.cpp
+++ Cometa.cpp
@@ -1,8 +1,68 @@
 #include "Cometa.h"
+string cometa_valid(int a, int variable)
+{
+    int cont = 0;
+    int value = a;
+    while (value != 0) {
+        value /= 10;
+        cont++;
+    }
+    int x = cont;
+    string s = "", p = "", c5 = "";
 
+    for (size_t i = 0; i < x; i++)
+    {
+        if (x == 1)
+        {
+            s = "   " + to_string(a) + "   ";
+            p = " " + to_string(a) + " ";
+        }
+        else if (x == 2) {
+            s = "   " + to_string(a) + "  ";
+            p = " " + to_string(a);
+        }
+        else if (x == 3) {
+            s = "  " + to_string(a) + "  ";
+            p = to_string(a);
+        }
+        else if (x == 4) {
+            s = "  " + to_string(a) + " ";
+        }
+        else if (x == 5) {
+            s = " " + to_string(a) + " ";
+        }
+        else if (x == 6) {
+            s = to_string(a) + " ";
+        }
+        else if (x == 7) {
+            s = to_string(a);
+        }
+        else {
+            s = " !Valid";
+            break;
+        }
+
+    }
+
+    return (variable == 2) ? s : p;
+}
 void Cometa::leerCometa(int a, int b, int d, int D)
 {
-    string linea;
+    int area = 2 * (a + b), suma = (a + b), mult = (D * d), perimetro = mult / 2; 
+    string value_a = cometa_valid(a, 1), value_b = cometa_valid(b, 1), 
+        value_d = cometa_valid(d, 1), value_D = cometa_valid(D,1),
+        value = cometa_valid(suma,2), value2 = cometa_valid(mult, 2);   
+
+    string str = R"([\[\]])", str2 = R"( \{a\+b\} )", str3 = R"(\(\{D\*d\}\))", 
+        str4 = R"([\{\}])", str5 = R"(\[\{2\*a\+b\}\])";
+
+    regex patron(str), val(str2), val2(str3),patron2(str4), patron3(str5); 
+
+    string str_a = R"(\{a\})", str_b = R"(\{b\})", str_D = R"(\{D\})", str_d = R"(\{d\})";  
+    regex patron_a(str_a), patron_b(str_b), patron_D(str_D), patron_d(str_d); 
+    
+    string linea; 
+
     ifstream Entrada("Cometa.txt", ios::in);
     if (!Entrada) {
         cerr << "No se pudo abrir el archivo" << endl;
@@ -12,7 +72,52 @@ void Cometa::leerCometa(int a, int b, int d, int D)
     {
         while (!Entrada.eof()) {
             getline(Entrada, linea);
-            cout << linea << endl;
+            smatch matches; 
+            if (regex_search(linea, matches, patron)) 
+            {
+                string llaves = regex_replace(linea, patron, " ");
+                 
+                if (regex_search(linea, matches, patron_a)) ///linea de formulas editar 
+                {  
+                    string values = regex_replace(llaves, patron_a, value_a);  
+                    string cadena = regex_replace(values, patron_b, value_b);
+                    values = regex_replace(cadena, patron_D, value_D); 
+                    cadena = regex_replace(values, patron_d, value_d);; 
+                    cout << cadena << endl;  
+                }
+                else if (regex_search(linea, matches, patron_b))   
+                {
+                    string values = regex_replace(llaves, patron_b, value_b); 
+                    string cadena = regex_replace(values,val,value);
+                    values = regex_replace(cadena, val2, value2); 
+                    cout << values << endl;     
+                }
+                else if (regex_search(linea, matches, patron_D)) 
+                {
+                    string values = regex_replace(llaves, patron_D, value_D); 
+                    cout << values << endl; 
+                }
+                else if (regex_search(linea, matches, patron_d)) 
+                {
+                    string values = regex_replace(llaves, patron_d, value_d); 
+                    cout << values << endl;
+                }
+                else if (regex_search(linea, matches, patron2)) 
+                {
+                    string values = regex_replace(llaves, patron_d, value_d);
+                    cout << values << endl;
+                }
+                else {
+                    cout << llaves << endl; 
+                }
+
+                /*string resultado = regex_replace(llaves, patron, " ");
+
+                cout << resultado << endl;*/ 
+            }
+            else {
+                cout << linea << endl;
+            }
         }
         Entrada.close();
     }
